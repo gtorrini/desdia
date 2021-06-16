03e76707babdf78c70321eec84045b5df323f7cc
Gaby Torrini <gtorrini@des71.fnal.gov>
2021-06-16 12:53:41 -0500

    Committer: Gaby Torrini <gtorrini@des71.fnal.gov>
    On branch gaby_des71
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	modified:   sourceme.sh
            Commit: I edited the sourceme.sh file to explicitly specify which package versions to setup based on my EUPS environment.

--- sourceme.sh
+++ sourceme.sh
@@ -2,9 +2,9 @@
 source ${HOME}/eups/desdm_eups_setup.sh
 export DES_SERVICES=${HOME}/.desservices.ini
 setup despydb 2.0.6+0
-setup despymisc
-setup diffimg
-setup swarp
-setup sextractor
-setup astropy
-setup numpy
+setup despymisc 1.0.4+5
+setup diffimg 9.0.7+0
+setup swarp 2.40.1+0
+setup sextractor 2.21.0+1
+setup astropy 1.1.2+7
+setup numpy 1.13.3+0
