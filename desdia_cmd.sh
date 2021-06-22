5051feb48755beec956d609d0cea368ab201b855
gtorrini <torrini2@illinois.edu>
2021-06-22 12:53:00 -0500

    Updated scripts for running 1 pointing vs 1 ccd   Changes to be committed: 	modified:   desdia_cmd.sh 	new file:   desdia_pnt.sh

--- desdia_cmd.sh
+++ desdia_cmd.sh
@@ -1,1 +1,1 @@
-./desdia --survey 1 -c 1 -n 30 -w /data/des80.a/data/${USER}/ --nowarn --offset
+./offset --survey 1 -c 1 -n 30 -s 3 -w /data/des80.a/data/${USER}/ --nowarn --offset
---
+++ desdia_pnt.sh
@@ -0,0 +1,1 @@
+./offset --survey 1 -n 30 -s 3 -w /data/des80.a/data/${USER}/ --nowarn --offset
